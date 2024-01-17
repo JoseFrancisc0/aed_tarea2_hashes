c9a709704ea85f3a87d548fc803dc5d8e5a49cad
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-01-17 10:45:04 -0500

    Initialized class header files

---
+++ hashtable.h
@@ -0,0 +1,6 @@
+#ifndef SEMANA_2_HASH_TABLE_H
+#define SEMANA_2_HASH_TABLE_H
+
+class HashTable{};
+
+#endif //SEMANA_2_HASH_TABLE_H
---
+++ set.h
@@ -0,0 +1,6 @@
+#ifndef SEMANA_2_SET_H
+#define SEMANA_2_SET_H
+
+class Set{};
+
+#endif //SEMANA_2_SET_H
