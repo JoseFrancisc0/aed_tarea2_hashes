85d86287858971bc5f5d4971316319c1d13d8781
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-01-18 22:14:15 -0500

    Set skeleton

--- set.h
+++ set.h
@@ -1,6 +1,19 @@
 #ifndef SEMANA_2_SET_H
 #define SEMANA_2_SET_H
 
-class Set{};
+template<typename T>
+class Set{
+    private:
+        // TODO: members
+    public:
+        Set();
+        void insert(T data);
+        bool remove(T data);
+        bool contains(T data);
+        Set Union(const Set& other);
+        Set Intersect(const Set& other);
+        Set Difference(const Set& other);
+        ~Set();
+};
 
 #endif //SEMANA_2_SET_H
